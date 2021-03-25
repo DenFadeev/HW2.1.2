677a27debe4acc2d1dea58d8bab1925b086f135e
DenisFadeev <fadeev.82@mail.ru>
2021-03-25 16:45:29 +0100

    Initial Commit

---
+++ HW2.1.2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW2.1.2
+//
+//  Created by user186451 on 3/25/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
